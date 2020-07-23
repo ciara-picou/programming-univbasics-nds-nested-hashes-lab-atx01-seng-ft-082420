aca4ceccba094432db7a8936cac183928327e249
Ciara Picou <ciarapicou@att.net>
2020-07-23 15:52:13 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -8,10 +8,7 @@ BRUCE_G = { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
 def assembled_aoh
  array = [
-   
-   { name:  "Don Gately", occupation: "Live-in Staff" } ,
-   { name:  "Joelle van Dyne", occupation: "Radio Personality" } ,
-   { name:  "Pat Monteseian", occupation: "Staff" } ,
+  
    { name:  "Kate Gompert", occupation: "None" } , 
    { name:  "Bruce Green", occupation: "Fan of Mildred" }
 
@@ -36,8 +33,6 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
-  # Update the AoH data at row and key to have the value of new_value
-  # Return the updated AoH
    aoh[row][key] = new_value
    aoh
 end
